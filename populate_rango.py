2cafe21020996185d2bddd651f10375e99a918a4
Reagan Cordero <2941984c@student.gla.ac.uk>
2025-01-30 16:43:32 +0000

    Chapter 6 Exercises Completed (BEFORE TESTING)

--- populate_rango.py
+++ populate_rango.py
@@ -12,32 +12,40 @@ def populate():
     # Category, where each page has a respective Name/Title and URL
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views': 5},
 
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views': 27},
 
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'}
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views': 139}
     ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+        'views': 1},
 
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views': 8812},
 
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'}
+         'url':'http://www.tangowithdjango.com/',
+         'views': 9000}
     ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views': 50},
 
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'}
+         'url':'http://flask.pocoo.org',
+         'views': 111}
     ]
 
     # Short for Category
@@ -56,7 +64,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url']) 
+            add_page(c, p['title'], p['url'], p['views']) 
 
     # Printing out every page that we added, organized by their respective categories
     for c in Category.objects.all():
@@ -67,7 +75,7 @@ def populate():
 # Helper functions that do the heavy lifting w/ adding categories
 # Add a Page
 def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
     p.url = url
     p.views = views
     p.save()
