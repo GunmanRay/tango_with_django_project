70d46780287afd7d97b5e30108aa2e855e1da57a
Reagan Cordero <2941984c@student.gla.ac.uk>
2025-02-04 16:41:59 +0000

    Chapter 7.2 Implemented BEFORE starting exercises

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -39,3 +41,19 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid(): 
+            form.save(commit=True)
+
+            return redirect('/rango/')
+        
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
