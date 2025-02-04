9452f42a468d604b57d0f6f777b2801cbf9d2986
Reagan Cordero <2941984c@student.gla.ac.uk>
2025-02-04 18:00:28 +0000

    Chapter 7 Tests Passed and Code Altered to reflect it

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, 
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, 
                            help_text="Please Enter the Category Name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -15,13 +15,14 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, 
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, 
                            help_text="Please Enter the title of the page.")
-    url = forms.URLField(max_length=200, 
+    url = forms.URLField(max_length=Page.URL_MAX_LENGTH, 
                          help_text="Please Enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
+        model = Page
         #You can either exclude certain fields like this:
         exclude = ('category',)
         #Or specify the field names WITHOUT the fields you don't want to include
